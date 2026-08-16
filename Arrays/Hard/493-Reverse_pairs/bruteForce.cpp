51447f72c9355cb32175ae99844a0d5685abdb07
pinumallajoshithasree <pinumallajoshithasree@gmail.com>
2026-08-16 12:22:22 +0530

    Create bruteForce.cpp

---
+++ Arrays/Hard/493-Reverse_pairs/bruteForce.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPairs(vector<int>&a, int n) {
+
+    // Count the number of pairs:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > 2 * a[j]) cnt++;
+        }
+    }
+    return cnt;
+}
+
+int team(vector <int> & skill, int n) {
+    return countPairs(skill, n);
+}
+
+int main()
+{
+    vector<int> a = {4, 1, 2, 3, 1};
+    int n = 5;
+    int cnt = team(a, n);
+    cout << "The number of reverse pair is: "
+         << cnt << endl;
+    return 0;
+}
+
+// Complexity Analysis
+
+// Time Complexity: O(N2), We are using nested loops here and those two loops roughly run for N times.
+
+// Space Complexity: O(1), as we are not using any extra space to solve this problem.
+
